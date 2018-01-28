f7fa0426a9efbe51f221e4eb95dbf69a5bcc09b1
psyKing <contact@dennisking.de>
2018-01-28 22:34:59 +0100

    adds credits to the author of the project i forked to the comment

--- wp-content/plugins/symfony-service-loader/symfony-service-loader.php
+++ wp-content/plugins/symfony-service-loader/symfony-service-loader.php
@@ -2,19 +2,28 @@
 /**
  * Symfony Service Loader
  *
- * @author      psyKing
- * @copyright   2018 psyKing
+ * @author      ekino, psyKing
+ * @copyright   2018 ekino, psyKing
  * @license     GPL-2.0+
  *
  * @wordpress-plugin
  * Plugin Name: Symfony Service Loader
- * Plugin URI:
+ * Plugin URI:  https://github.com/psyKing/symfony-service-loader/
  * Description: Provides a Method that retrieves a Symfony
- *              service which you can specify by its service
- *              id as an argument.
+ *              service which you can specify as an argument
+ *              by its service id .
+ *              I forked the following project:
+ *              https://github.com/ekino/ekino-wordpress-symfony
+ *
+ *              I removed the hook-function, put the loading of the
+ *              symfony core into a class and made it work with
+ *              Symfony 3.4.
+ *
+ *              Thanks to ekino.
+ *
  * Version:     1.0.0
- * Author:      psyKing
- * Author URI:  https://github.com/psyKing/
+ * Author:      ekino, psyKing
+ * Author URI:  http://www.ekino.com, https://github.com/psyKing/
  * Text Domain: symfony-service-loader
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
